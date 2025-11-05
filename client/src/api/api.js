a0621651c52640be698a25f8679a2260e9c76ba2
RishikeshDwivedi-99 <rishikeshdwivedi99@gmail.com>
2025-11-05 14:29:29 +0530

    Update API URL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = "https://slot-swapper-api-vece.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
 });
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
     return config;
   },
